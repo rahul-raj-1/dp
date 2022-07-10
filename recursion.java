7fd9f4dd8057a69cb5d65f7ba8313c9ecbb73fe3
Rahul <superindians008@gmail.com>
2022-07-11 00:14:09 +0530

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,14 @@
+====================
+1)Print all power sets/Subsequence . (Same code for both of them) .
+Backtracking is used when you have multiple solutions and we want all solutions
+Q1) Find number of powerset/subsesequences of a String/Array and print them ?
+Ans1)Watch below to understand recusrion
+ https://www.youtube.com/watch?v=mEBEw_xScsE&t=832s
+https://www.youtube.com/watch?v=V0IgCltYgg4&t=753s
+https://www.youtube.com/watch?v=tWnHbSHwNmA&t=1246s
+
+2)Practice leetcode Letter Combinations of a Phone Number .Important question
+3)Practice leetcode 22. Generate Parentheses
+
+
+https://www.youtube.com/watch?v=YBSt1jYwVfU&list=PLl0KD3g-oDOGJUdmhFk19LaPgrfmAGQfo
